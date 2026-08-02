74d678c9730b4fc07923406b1636790ba1ba8af7
Eydev-bg <ansanalja@gmail.com>
2026-08-02 11:49:39 +0800

    Add PAC logo to all outgoing emails via custom mail theme header

---
+++ backend/resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,8 @@
+@props(['url'])
+<tr>
+    <td class="header">
+        <a href="{{ $url }}" style="display: inline-block;">
+            <img src="https://pac-alumni.vercel.app/pac-logo.jpg" class="logo" alt="Philippine Advent College">
+        </a>
+    </td>
+</tr>
